5a505b319bedf480ef0ae7ecc296794a00739ad6
Just-Rea <reatjie@gmail.com>
2021-07-21 10:54:22 +0200

    Update Task0-10.js

--- Task0-10.js
+++ Task0-10.js
@@ -1,16 +1,15 @@
-const str1 = 'Reamogetse';
-const str2 = 'Exceptional';
-
-function commonLetters(str1, str2){
-    common = []
-    for(let letter1 in str1){
-     for(let letter2 in str2){
-      if(letter2 === letter1){
-       common.push(letter2);
-      }
-     }
+function getCount(str1, str2){
+    let commonLetters = [];
+    let count = 0;
+    const object = str2.split("");
+    for (const str of str1){
+        let idx = object.findIndex(x => x === str);
+        if (idx >= 0){
+        commonLetters.push(object.find(x => x === str));
+            count++;
+            object.splice(idx, 1);
     }
-    return common;
-   }
-
-   commonLetters();
+    } 
+    return commonLetters;
+}
+console.log(getCount("reamogetse", "introvert"));
