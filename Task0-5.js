097a176346bd57d04c74783e5e6ac94941fa78b7
Just-Rea <reatjie@gmail.com>
2021-07-21 10:58:27 +0200

    Update Task0-5.js

--- Task0-5.js
+++ Task0-5.js
@@ -1,9 +1,6 @@
-//Formula: Heron's
-
 function areaOfTriangle(x, y ,z){
     let sides = (x + y + z) / 2;
     let area = Math.sqrt(sides * (sides - x) * (sides - y) * (sides - z));
     console.log(area);
 }
-
 areaOfTriangle(6, 8, 12);
